e111445b3686b8a992f19facd7f7ff2b321a9e6e
Marco Arnulfo <arnulfomarco7@gmail.com>
2024-05-12 16:42:42 +0200

    add server-side API to fetch status

---
+++ app/api/status/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+
+export async function GET(){
+    try {
+        const apiUrl = process.env.API_URL;
+        const apiKey = process.env.API_KEY;
+        console.log(apiKey)
+
+        if (!apiUrl || !apiKey) {
+            return NextResponse.json({status: 500, message: 'api url or api key not found '})
+        }
+
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'x-api-key': apiKey
+            }
+        })
+
+        const data = await response.json()
+
+
+        return NextResponse.json(data)
+    } catch (error) {
+        return NextResponse.json({status: 500, message: error})
+    }
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,28 @@
 "use client";
 import { Status } from "@/types/response.status";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  // console.log(process.env.API_URL)
-  // console.log(process.env.API_KEY)
-  
 
   const [status, setStatus] = useState(Status.Pending)
 
 
   async function checkStatus() {
     try {
-      const apiUrl = process.env.API_URL
-      const apiKey = process.env.API_KEY
-      if (apiUrl && apiKey) {
-        const response = await fetch(apiUrl, {
-          method: 'GET',
-          headers: {
-            'x-api-key': apiKey 
-          }
-        })
-        const data = await response.json();
-        console.log(data)
-      }
-    } catch (error) {
+      const response = await fetch('/api/status')
+      const data = await response.json()
+      console.log(data)
+      } catch (error) {
       console.error(error)
     }
   }
+
+
+  useEffect(() => {
+    checkStatus()
+  }, []);
+
+  
 
 
 
